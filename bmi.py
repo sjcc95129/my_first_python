11b0a51063c6168a8e7dac8206d92ec0c9649495
sjcc95129 <cc.huang@hotmail.com>
2026-02-16 13:31:38 -0800

    this is a commit message for the changes made to the bmi.py file. The commit includes the addition of a new category for BMI, which is "obese". The commit also includes a comment explaining the BMI categories.

---
+++ bmi.py
@@ -0,0 +1,12 @@
+height = float(input("Enter your height in meters: "))
+weight = float(input("Enter your weight in kilograms: "))
+bmi = weight / (height ** 2)
+print(f"Your BMI is: {bmi:.2f}")    
+if bmi < 18.5:
+    print("You are underweight.")
+elif 18.5 <= bmi < 25:
+    print("You have a normal weight.")
+elif 25 <= bmi < 30:
+    print("You are overweight.")
+else:
+    print("You are obese.") 
